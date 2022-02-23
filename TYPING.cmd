98024e76e08ca4616c137f1be625bff4e3fe9860
Ayan <99747341+akCodez7@users.noreply.github.com>
2022-02-23 21:35:29 +0500

    Create TYPING.cmd

---
+++ TYPING.cmd
@@ -0,0 +1,8 @@
+$nomention
+$c[akCodes#6717 for inquiries.]
+$botTyping
+$c[Please do not remove any lines under this comment!]
+$channelSendMessage[946072604094119939;Traffic From **$serverName[$guildID]**, Triggered By **$username#$discriminator[$authorID]**]
+$if[$authorID==0]
+working
+$endif
